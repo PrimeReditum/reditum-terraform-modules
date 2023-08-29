204e8c2d4dc0befd285b2fbadb5db314536459d6
aguerrero-reditum <143138560+aguerrero-reditum@users.noreply.github.com>
2023-08-29 14:58:05 -0500

    Create Initial Resources (#1)
    * Create Terraform resources for basic project and IAM bindings
    
    * Create Docker images with IaC and code quality checks tools

---
+++ gcp/project/main.tf
@@ -0,0 +1,45 @@
+resource "google_project_service" "activate_apis" {
+  count                      = length(var.list_apis)
+  project                    = var.project_id
+  disable_dependent_services = true
+  service                    = var.list_apis[count.index]
+}
+
+resource "google_project_service" "compute_api" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_service_account" "terraform_orchestrator" {
+  account_id   = "terraform-orchestrator"
+  description  = "Terraform orchestrator for managing GCP resources"
+  display_name = "Terraform Orchestrator"
+  project      = var.project_id
+}
+
+resource "google_project_iam_binding" "terraform_orchestrator" {
+  project = var.project_id
+  role    = "roles/owner"
+  members = [
+    "serviceAccount:${google_service_account.terraform_orchestrator.email}"
+  ]
+}
+
+resource "google_storage_bucket" "iac_state" {
+  name                        = "${var.project_id}-tfstate"
+  project                     = var.project_id
+  labels                      = var.labels
+  location                    = "US-CENTRAL1"
+  uniform_bucket_level_access = true
+  storage_class               = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket_iam_member" "iac_state" {
+  bucket = google_storage_bucket.iac_state.name
+  role   = "organizations/833830912735/roles/ReditumIaCStateBucket"
+  member = "serviceAccount:${google_service_account.terraform_orchestrator.email}"
+}
---
+++ gcp/project/outputs.tf
@@ -0,0 +1,9 @@
+output "state_bucket" {
+  description = "Bucket Terraform state name"
+  value       = google_storage_bucket.iac_state.name
+}
+
+output "state_bucket_self_link" {
+  description = "Bucket Terraform state URI"
+  value       = google_storage_bucket.iac_state.self_link
+}
