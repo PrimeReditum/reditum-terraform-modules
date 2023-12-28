a42e508928aca70c2f9d7a86ba5d6282b7855536
aguerrero-reditum <143138560+aguerrero-reditum@users.noreply.github.com>
2023-12-28 17:58:48 -0500

    Add terraform_orquestrator SA email output for project module (#7)

--- gcp/project/outputs.tf
+++ gcp/project/outputs.tf
@@ -7,3 +7,8 @@ output "state_bucket_self_link" {
   description = "Bucket Terraform state URI"
   value       = google_storage_bucket.iac_state.self_link
 }
+
+output "terraform_orchestrator_email" {
+  description = "Terraform Orchestrator service account email"
+  value       = google_service_account.terraform_orchestrator.email
+}
