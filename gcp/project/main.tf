da18c4d0aee2d1d5d8adbdbb8af20b8f80b2d18a
Alejandro Guerrero <aguerrero@reditum.ec>
2024-06-21 20:21:21 -0500

    [IT-50] Disable soft delete feature for buckets

--- gcp/project/main.tf
+++ gcp/project/main.tf
@@ -1,15 +1,17 @@
 resource "google_compute_project_metadata" "compute_metadata" {
   depends_on = [google_project_service.compute_api]
+
   metadata = {
     enable-oslogin = "TRUE"
   }
+
   project = var.project_id
 }
 
 resource "google_project_service" "activate_apis" {
   count                      = length(var.list_apis)
-  project                    = var.project_id
   disable_dependent_services = true
+  project                    = var.project_id
   service                    = var.list_apis[count.index]
 }
 
@@ -26,12 +28,17 @@ resource "google_service_account" "terraform_orchestrator" {
 }
 
 resource "google_storage_bucket" "iac_state" {
-  name                        = "${var.project_id}-tfstate"
-  project                     = var.project_id
-  labels                      = var.labels
-  location                    = "US-CENTRAL1"
-  uniform_bucket_level_access = true
+  labels   = var.labels
+  name     = "${var.project_id}-tfstate"
+  project  = var.project_id
+  location = "US-CENTRAL1"
+
+  soft_delete_policy {
+    retention_duration_seconds = 0
+  }
+
   storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
 
   versioning {
     enabled = true
@@ -40,6 +47,6 @@ resource "google_storage_bucket" "iac_state" {
 
 resource "google_storage_bucket_iam_member" "iac_state" {
   bucket = google_storage_bucket.iac_state.name
-  role   = "organizations/833830912735/roles/ReditumIaCStateBucket"
   member = "serviceAccount:${google_service_account.terraform_orchestrator.email}"
+  role   = "organizations/833830912735/roles/ReditumIaCStateBucket"
 }
